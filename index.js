120bf77c57304a2f92bc11fd5cb836805fae8041
sanchezju <sjuan634@yahoo.com>
2022-12-11 18:50:39 -0800

    add logic to renderLicense*

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const questions = [{
   type: 'input',
   name: 'projTitle',
-  message: `Enter your project's title`
+  message: `Enter your project's title\n`
 },
 {
   type: 'input',
@@ -17,29 +17,29 @@ Use the following questions as a guide:
 - What was your motivation?
 - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
 - What problem does it solve?
-- What did you learn?`
+- What did you learn?\n`
 },
 {
   type: 'input',
   name: 'projInstall',
-  message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+  message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.\n'
 },
 {
   type: 'input',
   name: 'projUsage',
-  message: 'Provide instructions on how to use the project'
+  message: 'Provide instructions on how to use the project\n'
 },
 {
   type: 'confirm',
   name: 'confirmLic',
   message: `Do you want to add a license to your project?
-If you need help choosing a license, refer to https://choosealicense.com/`
+If you need help choosing a license, refer to https://choosealicense.com/\n`
 },
 {
   type: 'list',
   name: 'projLic',
   message: `Choose a license.
-If you need help choosing a license, refer to https://choosealicense.com/`,
+If you need help choosing a license, refer to https://choosealicense.com/\n`,
   choices: ['Apache License 2.0',
             'GNU General Public License v3.0',
             'MIT License',
@@ -47,28 +47,33 @@ If you need help choosing a license, refer to https://choosealicense.com/`,
             'BSD 3-Clause "New" or "Revised" License',
             'Boost Software License 1.0',
             'Creative Commons Zero v1.0 Universal',
-            'Eclipse Public License 2.0',
+            'Eclipse Public License 1.0',
             'GNU Affero General Public License v3.0',
             'GNU General Public License v2.0',
-            'GNU Lesser General Public License v2.1',
+            'GNU Lesser General Public License v3.0',
             'Mozilla Public License 2.0',
             'The Unlicense'],
   when: (answers) => answers.confirmLic === true
 },
 {
   type: 'input',
+  name: 'c',
+  message: 'How can other contribute to this project?\n'
+},
+{
+  type: 'input',
   name: 'projTest',
-  message: 'Provide any tests for you project.'
+  message: 'Provide any tests for you project.\n'
 },
 {
   type: 'input',
   name: 'github',
-  message: 'Enter your Github username?'
+  message: 'Enter your Github username?\n'
 },
 {
-  type: 'email',
+  type: 'input',
   name: 'email',
-  message: 'Enter your email'
+  message: 'Enter your email\n'
 }];
 
 // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,149 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licBadge = ``;
+  switch (license) {
+    case 'Apache License 2.0':
+      licBadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'GNU General Public License v3.0':
+      licBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'MIT License':
+      licBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'BSD 2-Clause "Simplified" License':
+      licBadge = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'BSD 3-Clause "New" or "Revised" License':
+      licBadge = `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'Boost Software License 1.0':
+      licBadge = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'Creative Commons Zero v1.0 Universal':
+      licBadge = `[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'Eclipse Public License 1.0':
+      licBadge = `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'GNU Affero General Public License v3.0':
+      licBadge = `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'GNU General Public License v2.0':
+      licBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'GNU Lesser General Public License v3.0':
+      licBadge = `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'Mozilla Public License 2.0':
+      licBadge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](${renderLicenseLink(license)})`;
+      break;
+    case 'The Unlicense':
+      licBadge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](${renderLicenseLink(license)})`;
+      break;
+    default:
+      licBadge = '';
+  }
+
+  return licBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licLink = ``;
+  switch (license) {
+    case 'Apache License 2.0':
+      licLink = `https://opensource.org/licenses/Apache-2.0`;
+      break;
+    case 'GNU General Public License v3.0':
+      licLink = `https://www.gnu.org/licenses/gpl-3.0`;
+      break;
+    case 'MIT License':
+      licLink = `https://opensource.org/licenses/MIT`;
+      break;
+    case 'BSD 2-Clause "Simplified" License':
+      licLink = `https://opensource.org/licenses/BSD-2-Clause`;
+      break;
+    case 'BSD 3-Clause "New" or "Revised" License':
+      licLink = `https://opensource.org/licenses/BSD-3-Clause`;
+      break;
+    case 'Boost Software License 1.0':
+      licLink = `https://www.boost.org/LICENSE_1_0.txt`;
+      break;
+    case 'Creative Commons Zero v1.0 Universal':
+      licLink = `http://creativecommons.org/publicdomain/zero/1.0/`;
+      break;
+    case 'Eclipse Public License 1.0':
+      licLink = `https://opensource.org/licenses/EPL-1.0`;
+      break;
+    case 'GNU Affero General Public License v3.0':
+      licLink = `https://www.gnu.org/licenses/gpl-3.0`;
+      break;
+    case 'GNU General Public License v2.0':
+      licLink = `https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`;
+      break;
+    case 'GNU Lesser General Public License v3.0':
+      licLink = `https://www.gnu.org/licenses/lgpl-3.0`;
+      break;
+    case 'Mozilla Public License 2.0':
+      licLink = `https://opensource.org/licenses/MPL-2.0`;
+      break;
+    case 'The Unlicense':
+      licLink = `http://unlicense.org/`;
+      break;
+    default:
+      licLink = '';
+  }
+
+  return licLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  !license ? '' : `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projTitle}
 
+  ${renderLicenseBadge(data.projLic)}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  
+  ## Description
+  ${data.projDesc}
+
+  ## Installation
+  ${data.projInstall}
+
+  ## Usage
+  ${data.projUsage}
+
+  ${renderLicenseSection(data.projLic)}
+
+  ## Contributing
+  ${data.projContribute}
+
+  ## Tests
+  ${data.projTest}
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
 `;
 }
 
